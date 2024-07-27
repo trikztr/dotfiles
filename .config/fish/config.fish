c8264a3415cfc7cd05de32a56067642a4ab82b71
Trym Zimmer <trym.zimmer@protonmail.com>
2024-07-27 23:32:23 +0200

    fix(fish): add .cargo/bin to path

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -7,4 +7,5 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 
-fish_add_path /home/trikztr/.spicetify
+fish_add_path $HOME/.cargo/bin
+fish_add_path $HOME/.spicetify
